2255156a960cab3295f7b026e77774a54e8198af
blinority <blinority@gmail.com>
2017-09-19 11:26:05 +0200

    fix

--- src/main/java/ba/dextractor/DuplicateExtraktorCont.java
+++ src/main/java/ba/dextractor/DuplicateExtraktorCont.java
@@ -14,24 +14,29 @@ import org.streampipes.wrapper.flink.FlinkSepaRuntime;
 public class DuplicateExtraktorCont extends AbstractFlinkAgentDeclarer<DuplicateExtraktoParam> {
 
 	public SepaDescription declareModel() {
-		return ProcessingElementBuilder.create("duplicateExtractor", "Duplicat Extractor", "Only let new Data trough")
-				.stream1PropertyRequirementWithUnaryMapping(EpRequirements.anyProperty(), "property-filter", "The property to filter", "description" )
-				.stream1PropertyRequirementWithUnaryMapping(EpRequirements.timestampReq(), "Timestamp", "Timestamp to compare", "description" )
+		return ProcessingElementBuilder
+				.create("duplicateExtractor", "Duplicat Extractor",
+						"Compares the Timestamps for a specific ID and sends the Data through when it is new and not in the Timewindow intervall. The Timestamp must be in Unix-Format")
+				.stream1PropertyRequirementWithUnaryMapping(EpRequirements.anyProperty(), "property-filter",
+						"The property to filter", "Must be a identifier")
+				.stream1PropertyRequirementWithUnaryMapping(EpRequirements.timestampReq(), "Timestamp",
+						"Timestamp to compare", "Must be in Unix-Time format")
 				.requiredIntegerParameter("time-window-value", "Time in seconds the new Value must be newer", "")
-				.outputStrategy(OutputStrategies.keep())
-				.supportedProtocols(SupportedProtocols.kafka()).supportedFormats(SupportedFormats.jsonFormat()).build();
+				.outputStrategy(OutputStrategies.keep()).supportedProtocols(SupportedProtocols.kafka())
+				.supportedFormats(SupportedFormats.jsonFormat()).build();
 	}
 
 	@Override
 	protected FlinkSepaRuntime<DuplicateExtraktoParam> getRuntime(SepaInvocation arg0) {
-		
+
 		ProcessingElementParameterExtractor extractor = ProcessingElementParameterExtractor.from(arg0);
-		
+
 		String propertyName = extractor.mappingPropertyValue("property-filter");
 		String timestamp = extractor.mappingPropertyValue("Timestamp");
 		Integer timeWindowValue = extractor.singleValueParameter("time-window-value", Integer.class);
-		
-		DuplicateExtraktoParam staticParams = new DuplicateExtraktoParam(arg0, propertyName, timeWindowValue, timestamp);
+
+		DuplicateExtraktoParam staticParams = new DuplicateExtraktoParam(arg0, propertyName, timeWindowValue,
+				timestamp);
 		return new DuplicateExtraktorProgramm(staticParams);
 	}
 
